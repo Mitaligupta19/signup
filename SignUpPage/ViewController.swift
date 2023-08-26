5088a200f424b03e4aad4ab338f6395b077118fe
mitali <mitali@orientaloutsourcing.com>
2023-08-26 16:13:59 +0530

    Initial Commit

---
+++ SignUpPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SignUpPage
+//
+//  Created by Mitali Gupta on 21/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
